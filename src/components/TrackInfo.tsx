4a443ae0ed02d68cda854193f4338b37444ecad0
Leo wilis <leowilis9898@gmail.com>
2026-03-20 18:39:19 +0700

    feat: add track info component

---
+++ src/components/TrackInfo.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { PlayerState, Track } from "@/hooks/Types";
+
+// TYPE
+type TrackInfoProps = {
+    track: Track;
+    state: PlayerState;
+};
+
+
+export function TrackInfo({ track, state }: TrackInfoProps) {
+  return (
+    <div className="flex flex-1 flex-col gap-[6px]">
+      <div className="space-y-1">
+        <h3 className="text-[18px] leading-snug font-semibold text-[var(--color-neutral-25)]">
+          {track.title}
+        </h3>
+        <p className="text-[14px] text-[var(--color-neutral-400)]">
+          {track.artist}
+        </p>
+      </div>
+      <Equalizer state={state} />
+    </div>
+  );
+}
